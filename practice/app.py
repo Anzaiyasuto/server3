e2fa89feaedd8137d47db815e8fa817d0cc71fdf
Anzaiyasuto <al18011@shibaura-it.ac.jp>
2021-12-29 14:07:26 +0900

    20211229v11

---
+++ practice/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import csv
+
+app = Flask(__name__)
+data = []
+
+@app.route("/index")
+def index():
+    csv_content = read_csv("AirPassengers")
+    for row in csv_content:
+        data.append(row[0])
+        print(data)
+    return render_template("index.html",input_from_python= data) # templatesフォルダ内のindex.htmlを表示する
+
+def read_csv(filename):
+    csv_file = open("./csv/" + str(filename) + ".csv", "r", encoding="ms932", errors="", newline="" )
+    f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+    #f = csv.DictReader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+    return f
+
+if __name__ == "__main__":
+    app.run()
--- test/test3.py
+++ test/test3.py
@@ -1,5 +1,28 @@
-list = ['A', 'B', 'C', 1, 2, 3]
+# Generate a fake csv file.
 
-map = map(str, list)
-print(''.join(map))
-# ABC123
+import datetime
+import random
+
+file = open("data.csv", "w")
+
+# header
+file.write("date,data1,data2\n")
+
+# data
+date = datetime.datetime.now()
+data1 = 0.5
+data2 = 0.5
+
+for i in range(100):
+    date = date + datetime.timedelta(seconds=1)
+    data1 = data1 + random.uniform(-0.01,0.01)
+    data2 = data2 + random.uniform(-0.01,0.01)
+    
+    file.write(date.strftime("%Y/%m/%d %H:%M:%S"))
+    file.write(",")
+    file.write(str(data1))
+    file.write(",")
+    file.write(str(data2))
+    file.write("\n")
+
+file.close()
