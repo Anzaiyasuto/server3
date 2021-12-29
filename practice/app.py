d006a4590b5e2f2e67c9065fdae6be2459e05dae
Anzaiyasuto <al18011@shibaura-it.ac.jp>
2021-12-29 21:49:24 +0900

    20211229v28

--- practice/app.py
+++ practice/app.py
@@ -1,23 +1,22 @@
 from flask import Flask, render_template
-from flask import request, jsonify, url_for, redirect
-from process import format_for_highcharts
+import csv
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+data = []
 
-import pandas as pd
-
-
-@app.route("/", methods=['GET'])
+@app.route("/index")
 def index():
-    return render_template('index.html')
+    csv_content = read_csv("AirPassengers")
+    for row in csv_content:
+        data.append(row[0])
+        print(data)
+    return render_template("index.html",input_from_python= data) # templatesフォルダ内のindex.htmlを表示する
 
-
-@app.route("/graph", methods=['GET'])
-def graph():
-    df = pd.read_csv('sample.csv', index_col=0)
-    return jsonify(format_for_highcharts(df))
-
+def read_csv(filename):
+    csv_file = open("./csv/" + str(filename) + ".csv", "r", encoding="ms932", errors="", newline="" )
+    f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+    #f = csv.DictReader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+    return f
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=18011, debug=True)
+    app.run(debug=True, host='0.0.0.0', port='18011')
